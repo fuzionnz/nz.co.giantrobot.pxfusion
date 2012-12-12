087752336d33c106043e4cae9eb62e001da5015c
Chris Burgess <chris@giantrobot.co.nz>
2012-12-12 14:02:54 +1300

    Introduce PxFusion.php as supplied by DPS.

---
+++ packages/PxFusion/PxFusion.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * @file
+ * PxFusion client library for PHP.
+ */
+
+/**
+ * Class for handling PxFusion transactions.
+ */
+class PxFusion
+{
+        // DPS Px Fusion Details
+        protected $fusion_username = '';
+        protected $fusion_password = '';
+        protected $wsdl = 'https://sec.paymentexpress.com/pxf/pxf.svc?wsdl';
+
+        // Variables/Objects that are used to hold data for transactions
+        public $tranDetail;
+        protected $soap_client;
+
+        public function __construct()
+        {
+                if ( ! is_object($this->tranDetail))
+                {
+                        $this->tranDetail = new stdClass();
+                }
+        }
+
+        public function set_txn_detail($property, $value)
+        {
+                $this->tranDetail->$property = $value;
+        }
+
+        public function get_transaction_id()
+        {
+                $this->soap_client = new SoapClient($this->wsdl, array('soap_version' => SOAP_1_1));
+
+                // SoapClient does some magic conversion from array into the required soap+xml format
+                $array_for_soap = array(
+                        'username' => $this->fusion_username,
+                        'password' => $this->fusion_password,
+                        'tranDetail' => get_object_vars($this->tranDetail) # extracts all properties of object into associative array
+                );
+
+                $response = $this->soap_client->GetTransactionId($array_for_soap);
+                return $response;
+        }
+
+        public function get_transaction($transaction_id)
+        {
+                $this->soap_client = new SoapClient($this->wsdl, array('soap_version' => SOAP_1_1));
+                $array_for_soap = array(
+                        'username' => $this->fusion_username,
+                        'password' => $this->fusion_password,
+                        'transactionId' => $transaction_id
+                );
+
+                $response = $this->soap_client->GetTransaction($array_for_soap);
+                return $response;
+        }
+}
